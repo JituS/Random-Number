8a91edc41dbf2e77ff1be27f2f65c3e6cc1425a6
jitendra sachdeva <jitendrs@thoughtworks.com>
2016-11-06 00:04:15 +0530

    Exercise4, Exercise5 has been done

---
+++ Exercise4/numberBox.js
@@ -0,0 +1,17 @@
+function visualize() {
+	var data = [0,1,2,3,4,5,6,7,8,9,10];
+	var svg = d3.select('#numberSize')
+		.style('width', '800px')
+		.style('height', '300px');
+
+	var scale = d3.scaleLinear()
+		.range([12, 120])
+		.domain([0, 10]);
+
+	svg.selectAll('div').data(data).enter()
+		.append('div')
+		.style('font-size', function(d, i){console.log(scale(d)); return scale(d) + 'px'})
+		.text(function(d, i){return d;})
+}
+
+window.onload = visualize;
---
+++ Exercise5/table.js
@@ -0,0 +1,25 @@
+var data = [1,2,3,4,5,6,7,8,9,10];
+
+function appendRow(scale, title) {
+	var table = d3.select('#tableContent table');
+	table = table.append('tr')
+	table.append('td').text(title);
+	table.selectAll('td').data(data, function(d){return d}).enter()
+		.append('td')
+		.text(function(d){return scale(d)});
+}
+
+function nLogRound(number) {
+	return Math.round(nLog(number));
+}
+
+function visualize() {
+	var table = d3.select('#tableContent').append('table');
+	appendRow(function(d){return d}, 'Title');
+	appendRow(d3.scaleLinear(), 'n');
+	appendRow(d3.scalePow().exponent(2), 'n Square');
+	appendRow(function(d){return d3.scaleLog()(d).toFixed(5)}, 'Log(n)');
+	appendRow(function(d){return Math.round(d3.scaleLog()(d))}, 'Log(n) Rounded');
+}
+
+window.onload = visualize;
